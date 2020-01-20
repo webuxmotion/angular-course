f03e173c61443934600093a0934ec3e11a28be4d
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-01-20 16:17:17 +0200

    Create component with angular-cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {PostComponent} from './post/post.component';
+import { Post2Component } from './post2/post2.component';
+import { Post3Component } from './post3/post3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    Post2Component,
+    Post3Component
   ],
   imports: [
     BrowserModule
