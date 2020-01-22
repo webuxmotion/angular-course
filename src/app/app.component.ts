75fced8b1b2b6ac248b65f4afc9935179f6815a1
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-01-22 14:59:06 +0200

    Lesson 14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-course';
+
+  inputValue = ''
+  title = 'Dynamic title'
+  number = 42
+  arr = [1, 2, 3]
+  obj = { a: 1, b: { c: 2 } }
+  img = 'https://www.stickpng.com/assets/images/584830f5cef1014c0b5e4aa1.png'
+
+  constructor() {
+    setTimeout(() => {
+      this.img = 'https://cdn4.iconfinder.com/data/icons/logos-and-brands-1/512/21_Angular_logo_logos-512.png'
+    }, 1000)
+  }
+
+  onInput(event: KeyboardEvent) {
+    this.inputValue = (<HTMLInputElement>event.target).value
+  }
+
+  onClick() {
+    console.table('clickddd')
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str
+  }
 }
