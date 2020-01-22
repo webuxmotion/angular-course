bc40b1d01136fa4a2d63298ee0d01e064582596b
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-01-22 15:58:35 +0200

    Lesson 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,28 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  inputValue = ''
-  title = 'Dynamic title'
-  number = 42
-  arr = [1, 2, 3]
-  obj = { a: 1, b: { c: 2 } }
-  img = 'https://www.stickpng.com/assets/images/584830f5cef1014c0b5e4aa1.png'
-
-  constructor() {
-    setTimeout(() => {
-      this.img = 'https://cdn4.iconfinder.com/data/icons/logos-and-brands-1/512/21_Angular_logo_logos-512.png'
-    }, 1000)
-  }
-
-  onInput(event: KeyboardEvent) {
-    this.inputValue = (<HTMLInputElement>event.target).value
-  }
-
-  onClick() {
-    console.table('clickddd')
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str
-  }
+  toggle = false
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {PostComponent} from './post/post.component';
@@ -16,7 +17,8 @@ import { Post4Component } from './post4/post4.component';
     Post4Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
